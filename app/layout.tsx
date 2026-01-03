e6f6e18e62ef72e4aafde956f635d2a182a87a7b
Hugo Lucero <h.lucero@itecriocuarto.org.ar>
2026-01-03 12:31:08 -0300

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter, JetBrains_Mono } from "next/font/google"
-// import { Analytics } from "@vercel/analytics/next"
+/// import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 const inter = Inter({
